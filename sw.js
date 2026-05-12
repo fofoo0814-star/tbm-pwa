c8024943f7b4986461f17f606550af23bc814d6f
fofoo0814-star <fofoo0814@gmail.com>
2026-05-12 23:50:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = "tbm-pwa-v1";
+const CACHE_NAME = "tbm-pwa-v2";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./data.js",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
