cb90271790677350eee12ec41f5e779c5032ec96
fofoo0814-star <fofoo0814@gmail.com>
2026-05-26 21:54:58 +0900

    Clear old cache and use network only

--- sw.js
+++ sw.js
@@ -1,29 +1,16 @@
-const CACHE_NAME = "tbm-pwa-v20";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./data.js",
-  "./manifest.json"
-];
+const CACHE_NAME = "tbm-pwa-v25";
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
